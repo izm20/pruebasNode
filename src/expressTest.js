e428c6cd6c824b281e01cf353f0f92f22bd273b9
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-11-10 17:45:02 +0000

    Mejorado express con endpoints

--- src/expressTest.js
+++ src/expressTest.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
+const users = [{id: 1, name: "Pepe"}, {id: 2, name: "Juan"}];
+
+app.get('/users/:id', (req, res) => {
+  const userId = Number(req.params.id);
+  const user = users.find(user => user.id === userId);
+  res.json(user);
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+
+app.get("/test", (req, res) => {
+  users[0].name = "Imar";  
+  res.json(users[0]);
+});
 
 app.get("/", (req, res) => {
   console.log("Request:", req);
-  res.json({saludo: "Hello Wordl!"});
+  res.json(users[0]);
 });
 
 app.listen(3000, () => {
